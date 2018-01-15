9ef19bcdaa267d951b3fae716b0dd5f62cb7429e
dsv-sergey <dsv-sergey@mail.ru>
2018-01-16 00:18:31 +0300

    add carousel and more

---
+++ js/owlCarousel.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    $('#about-carousel').owlCarousel({
+        loop: true,
+        nav: true,
+        navText: false,
+        items: 1,
+        responsiveClass: true,
+        responsive: {
+            0: {nav: false, navText: false},
+            992: {nav: true, navText: false, items: 1}
+        }
+    });
+});
