471549d74267c59fdadcdcac576bd87430d684a9
dsv-sergey <dsv-sergey@mail.ru>
2018-01-20 05:23:26 +0300

    fin

--- js/owlCarousel.js
+++ js/owlCarousel.js
@@ -1,3 +1,4 @@
+
 $(document).ready(function() {
     $('#about-carousel').owlCarousel({
         loop: true,
@@ -9,5 +10,15 @@ $(document).ready(function() {
             0: {nav: false, navText: false},
             992: {nav: true, navText: false, items: 1}
         }
+    });
+    $('#comments-carousel').owlCarousel({
+        loop:true,
+        nav:true,
+        navText:false,
+        items:1,
+        responsiveClass:true,
+        responsive:{
+            0:{nav:false, navText:false},
+            768:{nav:true, navText:false, items:1}}
     });
 });
